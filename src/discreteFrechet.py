278dcc9696e43dd9142c0ca94a72a5ace30b6a45
Fabian Kovacs <Fabian.Kovacs@koxi.de>
2016-12-05 14:19:29 +0100

    added work on path finding

--- src/discreteFrechet.py
+++ src/discreteFrechet.py
@@ -3,38 +3,37 @@ import numpy.linalg as linalg
 
 
 # Euclidean distance.
-def euc_dist(pt1,pt2):
-	return linalg.norm(pt1 - pt2)
+def euc_dist(p, q):
+    dt = (p[0] - p[0], p[1] - q[1])
+    return np.sqrt(dt[0] ** 2 + dt[1] ** 2)
 
-def _c(ca,i,j,P,Q):
-	if ca[i,j] > -1:
-		return ca[i,j]
-	elif i == 0 and j == 0:
-		ca[i,j] = euc_dist(P[0],Q[0])
-	elif i > 0 and j == 0:
-		ca[i,j] = max(_c(ca,i-1,0,P,Q),euc_dist(P[i],Q[0]))
-	elif i == 0 and j > 0:
-		ca[i,j] = max(_c(ca,0,j-1,P,Q),euc_dist(P[0],Q[j]))
-	elif i > 0 and j > 0:
-		ca[i,j] = max(min(_c(ca,i-1,j,P,Q),_c(ca,i-1,j-1,P,Q),_c(ca,i,j-1,P,Q)),euc_dist(P[i],Q[j]))
-	else:
-		ca[i,j] = float("inf")
-	return ca[i,j]
+
+def _frechet_dist(ca, i, j, P, Q):
+    if ca[i, j] > -1:
+        return ca[i, j]
+    elif i == 0 and j == 0:
+        ca[i, j] = euc_dist(P[0], Q[0])
+    elif i > 0 and j == 0:
+        ca[i, j] = max(_frechet_dist(ca, i - 1, 0, P, Q), euc_dist(P[i], Q[0]))
+    elif i == 0 and j > 0:
+        ca[i, j] = max(_frechet_dist(ca, 0, j - 1, P, Q), euc_dist(P[0], Q[j]))
+    elif i > 0 and j > 0:
+        ca[i, j] = max(min(_frechet_dist(ca, i - 1, j, P, Q), _frechet_dist(ca, i - 1, j - 1, P, Q), _frechet_dist(ca, i, j - 1, P, Q)),
+                       euc_dist(P[i], Q[j]))
+    else:
+        ca[i, j] = float("inf")
+    return ca[i, j]
+
 
 """ Computes the discrete frechet distance between two polygonal lines
 Algorithm: http://www.kr.tuwien.ac.at/staff/eiter/et-archive/cdtr9464.pdf
 P and Q are arrays of 2-element arrays (points)
 """
-def frechetDist(P,Q):
-	ca = np.multiply(np.ones((len(P),len(Q))), -1)
-
-	frechetDistance= _c(ca,len(P)-1,len(Q)-1,P,Q)
-	
-	return (frechetDistance , ca)
 
 
-_p = np.asarray([(1,0), (2,2), (-1,3), (2,4)])
-_q = np.asarray([(-3, 2), (-2,0), (0.5, 1)])
+def frechet_dist(P, Q):
+    ca = np.multiply(np.ones((len(P), len(Q))), -1)
 
+    dist = _frechet_dist(ca, len(P) - 1, len(Q) - 1, P, Q)
 
-frechet, dists = frechetDist(_p, _q)
+    return dist, ca
--- src/lexicographicFrechet.py
+++ src/lexicographicFrechet.py
@@ -1,16 +1,64 @@
 import numpy as np
 import numpy.linalg as linalg
+from src.discreteFrechet import *
+from typing import Tuple, List, Dict
+import math as math
+
+Point = Tuple[float, float]
+Curve = List[Point]
+Matrix = Dict[Tuple[int, int], float]
 
 '''
-	dists = frechetDistance Matrix
-	preds = predecessors of Points
+dists = frechetDistance Matrix
+preds = predecessors of Points
 '''
-def _lexicographicPath(dists, i, j):
-	
-	if i == 0 and j == 0:
-		return [(0, 0)]
 
-	neighbours = [(p,dists[p]) for p in [(i-1,j), (i-1,j-1),(i,j-1)] if p[0] >= 0 and p[1] >= 1]
-	(pi, pj), dist = neighbours[np.array([p[1] for p in neighbours]).argmin()]
 
-	return _lexicographicPath(dists, pi, pj).append((i, j))
+def euc_dist(p: Point, q: Point) -> float:
+    dt = (p[0] - p[0], p[1] - q[1])
+    return math.sqrt(dt[0] ** 2 + dt[1] ** 2)
+
+
+def _lexicographic_path(ca: Matrix, i: int, j: int, P: Curve, Q: Curve) -> Tuple[float, Curve]:
+    if i == 0 and j == 0:
+        ca[i, j] = euc_dist(P[0], Q[0])
+        return ca[i, j], [(0, 0)]
+
+    # All neighbouring cells inside the grid on a monotone path.
+    neighbours = [p
+                  for p in [(i - 1, j), (i - 1, j - 1), (i, j - 1)]
+                  if p[0] >= 0 and p[1] >= 0]
+
+    val = float("+inf")
+    preds = []
+
+    for (pi, pj) in neighbours:
+        dist, preds = _lexicographic_path(ca, pi, pj, P, Q)
+        if dist < val:
+            val = dist
+
+    dst = euc_dist(P[i], Q[j])
+
+    ca[i, j] = max([dst, val])
+
+    return ca[i, j], preds + [(i, j)]
+
+
+def lexicographic_path(P: Curve, Q: Curve) -> Tuple[float, Curve, np.matrix]:
+    ca = np.multiply(np.ones((len(P), len(Q))), -1)
+    dist, indices = _lexicographic_path(ca, len(P) - 1, len(Q) - 1, P, Q)
+
+    return dist, indices, ca
+
+
+ps = list(zip(np.arange(0, 3), np.zeros(6)))
+ps += reversed(ps)
+qs = list(zip(np.arange(0, 6), np.arange(0, 6)))
+
+dist, indices, terrain = lexicographic_path(ps, qs)
+
+
+print(dist)
+print(indices)
+print(terrain)
+
