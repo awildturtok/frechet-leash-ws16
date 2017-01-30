dbf4d6fb4db890d8b0a94e4189ab17a6d26f6d14
Jana <jana-k@gmx.net>
2017-01-30 16:40:57 +0100

    run python input string in c++

--- gui/tutorial/qml_chart_example/frechet/frechet.pro
+++ gui/tutorial/qml_chart_example/frechet/frechet.pro
@@ -2,12 +2,11 @@ TEMPLATE = app
 
 QT += widgets qml quick charts
 
-CONFIG += c++11
+CONFIG += c++11 noKeywords
 
 SOURCES += main.cpp
 
 RESOURCES += qml.qrc
-
 # Additional import path used to resolve QML modules in Qt Creator's code model
 QML_IMPORT_PATH =
 
@@ -15,3 +14,10 @@ QML_IMPORT_PATH =
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+win32:CONFIG(release): LIBS += -L../../../../../../AppData/Local/Programs/Python/Python35-32/libs/ -lpython35
+#else:win32:CONFIG(debug, debug|release): LIBS += -L../../../../../../AppData/Local/Programs/Python/Python35-32/libs/ -lpython35d
+else:unix: LIBS += -L../../../../../../AppData/Local/Programs/Python/Python35-32/libs/ -lpython35
+
+INCLUDEPATH += ../../../../../../AppData/Local/Programs/Python/Python35-32/libs
+DEPENDPATH += ../../../../../../AppData/Local/Programs/Python/Python35-32/libs
--- gui/tutorial/qml_chart_example/frechet/main.cpp
+++ gui/tutorial/qml_chart_example/frechet/main.cpp
@@ -1,23 +1,54 @@
-#include <QApplication>
+#include <../../../../../../AppData/Local/Programs/Python/Python35-32/include/Python.h>
 #include <QQmlApplicationEngine>
+#include <QApplication>
 #include <QtQuick>
 #include <QtCharts/QChartView>
+#include <cmath>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    QObject *window = engine.rootObjects().at(0);
-    QObject *chart = window->children().at(0);
+     QQuickView viewer;
+     // The following are needed to make examples run without having to install the module
+     // in desktop environments.
+    #ifdef Q_OS_WIN
+        QString extraImportPath(QStringLiteral("%1/../../../../%2"));
+    #else
+        QString extraImportPath(QStringLiteral("%1/../../../%2"));
+    #endif
 
-    /*transform coordinates ?! http://www.qcustomplot.com/index.php/support/forum/93
-     * http://doc.qt.io/qt-5/coordsys.html#window-viewport-conversion
-     * !!! http://stackoverflow.com/questions/18140446/display-the-plot-values-on-mouse-over-detect-scatter-points
-     *
-     */
-    //chart->
+    //import path define in viewer ?
+    viewer.engine()->addImportPath(extraImportPath.arg(QGuiApplication::applicationDirPath(),
+                                       QString::fromLatin1("qml")));
+
+    QObject::connect(viewer.engine(), &QQmlEngine::quit, &viewer, &QWindow::close);
+    viewer.setTitle(QStringLiteral("QML Curve Input"));
+
+    viewer.setSource(QUrl("qrc:/main.qml"));
+
+    //Py_SetProgramName(L'ab');  /* optional but recommended */
+    Py_Initialize();
+
+    ///////////////////////////////////
+    PyRun_SimpleString("result = 5 ** 2");
+    PyObject * module = PyImport_AddModule("__main__"); // borrowed reference
+    assert(module);                                     // __main__ should always exist
+    PyObject * dictionary = PyModule_GetDict(module);   // borrowed reference
+    assert(dictionary);                                 // __main__ should have a dictionary
+    PyObject * result  = PyDict_GetItemString(dictionary, "result");     // borrowed reference
+
+    assert(result);                                     // just added result
+    long result_value = PyLong_AsLong(result);       // already checked that it is an int
+
+    qDebug() << result_value;
+
+
+
+    //////////////////////////////////
+
+    Py_Finalize();
+
 
     return app.exec();
 }
