f6f8cb592ad35f50dc23e1e76bbc2c6e381044c2
mertens <mertens.josephine@web.de>
2017-02-01 17:53:48 +0100

    * test syscall for python script. * python must be installed on C. Set environment variable C:/Python/Pythonxx

--- gui/tutorial/qml_chart_exampleAlex/frechet/main.cpp
+++ gui/tutorial/qml_chart_exampleAlex/frechet/main.cpp
@@ -8,5 +8,11 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
+    std::string filename = "test.py";
+    std::string command = "python ";
+    command += filename;
+    system(command.c_str());
+
     return app.exec();
 }
+
